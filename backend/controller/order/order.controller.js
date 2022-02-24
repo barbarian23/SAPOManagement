8b476032197dfaa9222761a4fce118d95279f01d
Kong <congpv.infosec@gmail.com>
2022-02-25 00:22:37 +0700

    update service

--- backend/controller/order/order.controller.js
+++ backend/controller/order/order.controller.js
@@ -1,34 +1,13 @@
-import { OrderService } from "../../service";
+import {OrderService} from "../../service";
 import { responceJson } from '../../util';
-
-// export async function getOrders(req, res) {
-//       console.log("getOrders");
-//       const { body } = req;
-//       //const {param} = req;
-
-//       let result = await OrderService.getInstance().getAllOrder();
-//       console.log("result", result);
-//       responceJson(res, 200, { message: result });
-// }
 
 export async function getOrders(req, res, next) {
       // const { id } = req.params;
       try {
-            const result = await OrderService.getInstance().getAll();
-            // return res.status(response.statusCode).json(response);
+            const result = await OrderService.getAll();
             responceJson(res, 200, result);
       }
       catch (e) {
             next(e);
       }
-}
-
-export async function updateOrder(req, res) {
-      console.log("updateOrder");
-      const { body } = req;
-      //const {param} = req;
-
-      let result = await OrderService.getInstance().updateOrder(body);
-      console.log("result", result);
-      responceJson(res, 200, { message: result });
 }
---
+++ backend/service/iservice.js
@@ -0,0 +1,100 @@
+export default class IService {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async getAll() {
+        try {
+            let items = await this.model.find({});
+            return items
+        } catch (errors) {
+            throw errors;
+        }
+    }
+
+    async countAll() {
+        try {
+            let total = await this.model.countDocuments({});
+            return total;
+        } catch (errors) {
+            throw errors;
+        }
+    }
+
+    async count(query = {}) {
+        try {
+            let total = await this.model.countDocuments(query);
+            return total;
+        } catch (errors) {
+            throw errors;
+        }
+    }
+
+    async search(query = {}, skip, limit, sortBy) {
+        let _skip = skip ? Number(skip) : 0;
+        let _limit = limit ? Number(limit) : 10;
+        let _sortBy = sortBy ? sortBy : { createdAt: -1 };
+
+        try {
+            let items = await this.model
+                .find(query)
+                .sort(_sortBy)
+                .skip(_skip)
+                .limit(_limit);
+            return items;
+        } catch (errors) {
+            throw errors;
+        }
+    }
+
+    async get(id) {
+        try {
+            let item = await this.model.findById(id);
+            if (!item) {
+                throw error;
+            }
+            return item;
+        } catch (errors) {
+            throw errors;
+        }
+    }
+
+    async insert(data) {
+        try {
+            let item = await this.model.create(data);
+            if (item) {
+                return item;
+            } else {
+                throw new Error('Something wrong happened');
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async update(id, data) {
+        try {
+            let item = await this.model.findByIdAndUpdate(id, data, { new: true });
+            return item;
+        } catch (errors) {
+            throw errors;
+        }
+    }
+
+    async delete(id) {
+        try {
+            let item = await this.model.findByIdAndDelete(id);
+            if (!item) {
+                let error = new Error('Item not found');
+                throw error;
+            } else {
+                return {
+                    item: item,
+                    deleted: true
+                };
+            }
+        } catch (errors) {
+            throw errors;
+        }
+    }
+}
