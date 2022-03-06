65ef4c4f4520d62d675c91338ce0631768bdf18e
Đô Đô <thanhdo170698@gmail.com>
2022-03-06 22:43:55 +0700

    Update model

--- backend/job/order/order.job.js
+++ backend/job/order/order.job.js
@@ -1,4 +1,5 @@
 import { OrderService } from "../../service";
+import fetch from 'node-fetch';
 
 const Order = function (schedule) {
     var cronExpress = '*/10 * * * * * *';
@@ -6,6 +7,8 @@ const Order = function (schedule) {
         console.log(fireDate);
         let orderLast = await OrderService.getInstance().find().skip(0).limit(1).sort({created_on: -1}).exec();
         let createdOnLast = orderLast.created_on;
+        if(!createdOnLast)
+            createdOnLast = new Date();
     });
 }
 export default Order;
