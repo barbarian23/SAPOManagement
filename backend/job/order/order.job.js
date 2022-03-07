f527d356892f0d8c8bf98e6ba869b266f28e7a21
Đô Đô <thanhdo170698@gmail.com>
2022-03-08 00:43:11 +0700

    add new code job

--- backend/job/order/order.job.js
+++ backend/job/order/order.job.js
@@ -5,10 +5,15 @@ const Order = function (schedule) {
     var cronExpress = '*/10 * * * * * *';
     var j = schedule.scheduleJob(cronExpress, function(fireDate){
         console.log(fireDate);
-        let orderLast = await OrderService.getInstance().find().skip(0).limit(1).sort({created_on: -1}).exec();
-        let createdOnLast = orderLast.created_on;
-        if(!createdOnLast)
-            createdOnLast = new Date();
+        let orderCreatedLast = await OrderService.getInstance().find().skip(0).limit(1).sort({created_at: -1}).exec();
+        let createdLast = orderCreatedLast.created_at;
+        if(!createdLast)
+            createdLast = new Date();
+
+        let orderUpdatedLast = await OrderService.getInstance().find().skip(0).limit(1).sort({updated_at: -1}).exec();
+        let updatedLast = orderUpdatedLast.updated_at;
+        if(!updatedLast)
+            updatedLast = new Date();
     });
 }
 export default Order;
